9e4236d7ea27d1f4c34c5d75d08f0b2f5540ff30
slyguy5646 <lpmonaghan06@yahoo.com>
2025-09-05 18:34:25 -0400

    removed console logs

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -11,14 +11,10 @@ const app = new Hono();
 const REDIS_STORAGE_KEY = "locations";
 
 app.get("/", async (c) => {
-  console.log("GOING", new Date().toLocaleTimeString());
   let locationsByType: LocationsByType | null =
     await redis.get<LocationsByType>(REDIS_STORAGE_KEY);
 
-  if (locationsByType) console.log("CACHE HIT", locationsByType);
-
   if (!locationsByType) {
-    console.log("FETCHING");
     const { data, error } = await getData({ date: new Date() });
 
     const locations = data?.diningCourts;
@@ -30,7 +26,6 @@ app.get("/", async (c) => {
       quickBites: locations.filter((loc) => loc.category === "Quick Bites"),
     } satisfies LocationsByType;
 
-    console.log("SETTING CACHE");
     await redis.set(REDIS_STORAGE_KEY, locationsByType, {
       exat: getTomorrowMidnightUnix(),
     });
